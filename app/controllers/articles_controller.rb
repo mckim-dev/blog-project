b466a2daf714c97517aad4e6790ffcef33079a9b
student-mckim <mckim01@email.franklin.edu>
2021-09-17 11:08:30 -0400

    create action/article index/ f.field valid/flash

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,28 @@ class ArticlesController < ApplicationController
         @article = Article.find(params[:id])
     end
 
+    def index
+        @articles = Article.all
+    end
+
+    def new
+        @article = Article.new  #creates empty initialization to prevent error
+    end
+
+    def create
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+          flash[:notice] = "Your article has been created and saved." #flash hash message notices and alerts will appear within the body template of the app html
+          redirect_to @article
+        else
+          render 'new'
+        end
+    end
+
+    def update
+    end
+
+    def edit
+    end
+    
 end 
