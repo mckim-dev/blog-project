143ead6cd0a5586f6b71f21033467517e8d9abd3
student-mckim <mckim01@email.franklin.edu>
2021-09-21 15:29:45 -0400

    Styles, links, partials Added

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,8 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+#remove rails automatic layout style change
+ActionView::Base.field_error_proc = Proc.new do |html_tag, instance|
+    html_tag.html_safe
+end
