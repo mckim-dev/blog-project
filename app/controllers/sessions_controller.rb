b1767eefeb539002e6ef486e70d2454819fc8b88
student-mckim <mckim01@email.franklin.edu>
2021-09-25 17:54:14 -0400

    create/destroy sessions/auth helpers/NAV updates

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,24 @@
+class SessionsController < ApplicationController
+
+    def new
+    end
+
+    def create
+        user =User.find_by(email: params[:session] [:email].downcase)
+        if user && user.authenticate(params[:session][:password])
+            session[:user_id] = user.id
+            flash[:notice] = "Login Successful"
+            redirect_to user
+        else
+            flash.now[:alert] = "Your login details were incorrect "
+            render 'new'
+        end
+    end
+
+    def destroy
+        session[:user_id] = nil
+        flash[:notice] = "Logged Out"
+        redirect_to root_path
+    end
+
+end
