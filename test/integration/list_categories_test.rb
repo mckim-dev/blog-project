137d70a95536f951c3c8ea96aaf4c27024cfac1c
student-mckim <mckim01@email.franklin.edu>
2021-09-28 12:12:27 -0400

    fixed/passed list_cat int. tests

--- test/integration/list_categories_test.rb
+++ test/integration/list_categories_test.rb
@@ -3,14 +3,14 @@ require "test_helper"
 class ListCategoriesTest < ActionDispatch::IntegrationTest
 
   def setup
-    @category = Category.create("Cat1")
-    @category2 = Category.create("Cat2")
+    @category = Category.create(name: "Cat1")
+    @category2 = Category.create(name: "Cat2")
   end
 
   test "should show categories list" do 
     get '/categories'
-    assert_select "a[:href=?]", category_path(@category), text: category.name
-    assert_select "a[:href=?]", category_path(@category2), text: category2.name
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
   end
 
 end
